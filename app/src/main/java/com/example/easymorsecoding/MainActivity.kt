ea489dd23add4639a0d62ebc7e0624a3723f771e
davikokar <davide.seddio@gmail.com>
2026-08-14 10:57:07 +0200

    android icons white

--- app/src/main/java/com/example/easymorsecoding/MainActivity.kt
+++ app/src/main/java/com/example/easymorsecoding/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.WindowManager
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
@@ -30,7 +31,9 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.dark(android.graphics.Color.TRANSPARENT)
+        )
         setContent {
             val uiState by viewModel.uiState.collectAsState()
 
